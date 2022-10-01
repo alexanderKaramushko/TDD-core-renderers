18b5d970fb32514afdac4ea5aa7f29868aa70b96
Александр Карамушко <aleksandrkaramusko@MacBook-Air-Aleksandr.local>
2022-10-01 16:23:39 +0400

    update main test

--- src/tdd-methodology/main.test.ts
+++ src/tdd-methodology/main.test.ts
@@ -3,7 +3,7 @@ import main from './main';
 
 /* eslint-disable no-undef */
 describe('main', () => {
-  it('should move segment', () => {
+  it('should take app snapshot to detect undesirable changes', () => {
     document.body.innerHTML = '<div id="app"></div>';
 
     main();
